b0006e2db9cc4907bb85b2f6dc39e436ecd2cf01
Unknown <alex.pantec@gmail.com>
2018-01-26 19:27:31 +0200

    add comment

--- alexp/src/main/java/alexp/macrobase/utils/ThrowingConsumer.java
+++ alexp/src/main/java/alexp/macrobase/utils/ThrowingConsumer.java
@@ -1,5 +1,8 @@
 package alexp.macrobase.utils;
 
+/**
+ * The same as Java8 Consumer<T> but allows to throw any exception
+ */
 @FunctionalInterface
 public interface ThrowingConsumer<T> {
     void accept(T t) throws Exception;
