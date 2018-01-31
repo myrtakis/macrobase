c9ccef4728c855e1128b2fd1187150cb7da0ed3d
Unknown <alex.pantec@gmail.com>
2018-01-31 14:52:44 +0200

    windowed summarizer

--- alexp/src/main/java/alexp/macrobase/pipeline/StreamingPipeline.java
+++ alexp/src/main/java/alexp/macrobase/pipeline/StreamingPipeline.java
@@ -8,18 +8,23 @@ import edu.stanford.futuredata.macrobase.analysis.classify.Classifier;
 import edu.stanford.futuredata.macrobase.analysis.classify.PercentileClassifier;
 import edu.stanford.futuredata.macrobase.analysis.classify.PredicateClassifier;
 import edu.stanford.futuredata.macrobase.analysis.summary.Explanation;
+import edu.stanford.futuredata.macrobase.analysis.summary.fpg.FPGExplanation;
 import edu.stanford.futuredata.macrobase.analysis.summary.fpg.IncrementalSummarizer;
+import edu.stanford.futuredata.macrobase.datamodel.DataFrame;
 import edu.stanford.futuredata.macrobase.datamodel.Schema;
+import edu.stanford.futuredata.macrobase.operator.Operator;
+import edu.stanford.futuredata.macrobase.operator.WindowedOperator;
 import edu.stanford.futuredata.macrobase.pipeline.PipelineConfig;
 import edu.stanford.futuredata.macrobase.util.MacrobaseException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
 import java.util.function.Consumer;
 
 public class StreamingPipeline {
     private final Uri inputURI;
+
+    private String idColumn;
+    private String sqlQuery;
 
     private String classifierType;
     private String metric;
@@ -35,12 +40,20 @@ public class StreamingPipeline {
     private double minSupport;
 
     private Integer numPanes;
+    private Integer windowLength;
+    private Integer slideLength;
+    private String timeColumn;
 
     public StreamingPipeline(PipelineConfig conf) {
         inputURI = new Uri(conf.get("inputURI"));
 
         classifierType = conf.get("classifier", "percentile");
         metric = conf.get("metric");
+
+        if (inputURI.getType() == Uri.Type.JDBC) {
+            sqlQuery = conf.get("query");
+            idColumn = conf.get("idColumn", "id");
+        }
 
         if (classifierType.equals("predicate")) {
             Object rawCutoff = conf.get("cutoff");
@@ -64,13 +77,16 @@ public class StreamingPipeline {
         minSupport = conf.get("minSupport", 0.01);
 
         numPanes = conf.get("numPanes", 3);
+        windowLength = conf.get("windowLength", 6000);
+        slideLength = conf.get("slideLength", 1000);
+        timeColumn = conf.get("timeColumn", "time");
     }
 
     public void run(Consumer<Explanation> resultCallback) throws Exception {
         StreamingDataFrameLoader dataLoader = getDataLoader();
 
         Classifier classifier = getClassifier();
-        IncrementalSummarizer summarizer = getSummarizer(classifier.getOutputColumnName());
+        Operator<DataFrame, ? extends Explanation> summarizer = getSummarizer(classifier.getOutputColumnName());
 
         dataLoader.load(dataFrame -> {
             classifier.process(dataFrame);
@@ -91,6 +107,9 @@ public class StreamingPipeline {
         }
         List<String> requiredColumns = new ArrayList<>(attributes);
         requiredColumns.add(metric);
+        if (summarizerType.equals("windowed")) {
+            requiredColumns.add(timeColumn);
+        }
 
         switch (inputURI.getType()) {
             case HTTP:
@@ -125,7 +144,7 @@ public class StreamingPipeline {
         }
     }
 
-    private IncrementalSummarizer getSummarizer(String outlierColumnName) throws MacrobaseException {
+    private Operator<DataFrame, ? extends Explanation> getSummarizer(String outlierColumnName) throws MacrobaseException {
         switch (summarizerType.toLowerCase()) {
             case "incremental": {
                 IncrementalSummarizer summarizer = new IncrementalSummarizer();
@@ -134,6 +153,20 @@ public class StreamingPipeline {
                 summarizer.setMinSupport(minSupport);
                 summarizer.setWindowSize(numPanes);
                 return summarizer;
+            }
+            case "windowed": {
+                IncrementalSummarizer summarizer = new IncrementalSummarizer();
+                summarizer.setOutlierColumn(outlierColumnName);
+                summarizer.setAttributes(attributes);
+                summarizer.setMinSupport(minSupport);
+
+                WindowedOperator<FPGExplanation> windowedSummarizer = new WindowedOperator<>(summarizer);
+                windowedSummarizer.setWindowLength(windowLength);
+                windowedSummarizer.setSlideLength(slideLength);
+                windowedSummarizer.setTimeColumn(timeColumn);
+                windowedSummarizer.initialize();
+
+                return windowedSummarizer;
             }
             default: {
                 throw new MacrobaseException("Bad Summarizer Type");
