8f7e24d7c5f2bf4863eb9b2b0940a32cd5807808
Alex P <alex.pantec@gmail.com>
2018-04-23 16:31:46 +0300

    add mcod impl source code

---
+++ alexp/src/main/java/alexp/macrobase/outlier/mcod/mtree/MTreeClass.java
@@ -0,0 +1,37 @@
+package alexp.macrobase.outlier.mcod.mtree;
+
+import alexp.macrobase.outlier.mcod.mtree.tests.Data;
+import alexp.macrobase.outlier.mcod.mtree.utils.Pair;
+import alexp.macrobase.outlier.mcod.mtree.utils.Utils;
+
+import java.util.Set;
+
+public class MTreeClass extends MTree<Data> {
+
+    private static final PromotionFunction<Data> nonRandomPromotion = new PromotionFunction<Data>() {
+        @Override
+        public Pair<Data> process(Set<Data> dataSet, DistanceFunction<? super Data> distanceFunction) {
+            return Utils.minMax(dataSet);
+        }
+    };
+
+    public MTreeClass() {
+        super(25, DistanceFunctions.EUCLIDEAN, new ComposedSplitFunction<Data>(nonRandomPromotion,
+                new PartitionFunctions.BalancedPartition<Data>()));
+    }
+
+    public void add(Data data) {
+        super.add(data);
+        _check();
+    }
+
+    public boolean remove(Data data) {
+        boolean result = super.remove(data);
+        _check();
+        return result;
+    }
+
+    public DistanceFunction<? super Data> getDistanceFunction() {
+        return distanceFunction;
+    }
+}
---
+++ alexp/src/main/java/alexp/macrobase/outlier/mcod/mtree/utils/Constants.java
@@ -0,0 +1,23 @@
+package alexp.macrobase.outlier.mcod.mtree.utils;
+
+public class Constants {
+
+    public static double R = 1;
+    public static int k = 30;
+    public static int W = 1000;
+    public static int slide = 500;
+    public static int samplingPeriod = 100;
+    public static String forestCoverFileName = "C:\\Users\\Luan\\MTree\\covtype.data";
+    public static String taoFileName = "C:\\Users\\Luan\\MTree\\tao.txt";
+
+    public static String randomFileName1111 = "C:\\Users\\Luan\\MTree\\randomData1.txt";
+    public static String randomFileName001 = "C:\\Users\\Luan\\MTree\\Gauss0.001percentOutlier.txt";
+    public static String randomFileName01 = "C:\\Users\\Luan\\MTree\\Gauss0.01percentOutlier.txt";
+    public static String randomFileName1 = "C:\\Users\\Luan\\MTree\\Gauss0.1percentOutlier.txt";
+    public static String randomFileName1percent = "C:\\Users\\Luan\\MTree\\Gauss1percentOutlier.txt";
+    public static String randomFileName10percent = "C:\\Users\\Luan\\MTree\\Gauss10percentOutlier.txt";
+    public static String tagCALC = "C:\\Users\\Luan\\MTree\\tagCALC.txt";
+
+    public static String STT = "C:\\Users\\Luan\\MTree\\stt.txt";
+    public static String dataFile = "";
+}
