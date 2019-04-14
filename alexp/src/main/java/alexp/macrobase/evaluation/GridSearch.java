cef8760b5feb6954c82b38415d4196a2ab175daf
Alex P <alex.pantec@gmail.com>
2019-04-14 22:55:22 +0300

    gridsearch progress output

--- alexp/src/main/java/alexp/macrobase/evaluation/GridSearch.java
+++ alexp/src/main/java/alexp/macrobase/evaluation/GridSearch.java
@@ -1,6 +1,8 @@
 package alexp.macrobase.evaluation;
 
 import com.google.common.collect.Streams;
+
+import java.io.PrintStream;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -25,6 +27,8 @@ public class GridSearch {
 
     private SortedMap<Double, Map<String, Object>> results = new TreeMap<>();
 
+    protected PrintStream out = System.out;
+
     public GridSearch addParam(String name, Object[] values) {
         searchParams.add(new SearchParam(name, values));
         return this;
@@ -41,17 +45,26 @@ public class GridSearch {
 
         results.clear();
 
+        int num = 0;
         for (Object[] permutation : permutations) {
             // {"p1": x, "p2": y, ...}
             Map<String, Object> params = Streams.mapWithIndex(Arrays.stream(permutation), (o, i) -> new AbstractMap.SimpleEntry<>(searchParams.get((int) i).name, o))
                     .collect(Collectors.toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue));
 
-            results.put(runInstance.accept(params), params);
+            double score = runInstance.accept(params);
+
+            out.println(String.format("%d/%d %s -> %.4f", ++num, permutations.size(), params, score));
+
+            results.put(score, params);
         }
     }
 
     public SortedMap<Double, Map<String, Object>> getResults() {
         return results;
+    }
+
+    public void setOutputStream(PrintStream out) {
+        this.out = out;
     }
 
     private void generatePermutations(List<Object[]> paramsLists, List<Object[]> result, int depth, List<Object> current) {
