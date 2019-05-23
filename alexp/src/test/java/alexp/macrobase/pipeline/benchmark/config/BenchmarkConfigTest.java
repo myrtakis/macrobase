7edc08366e7c97a7b86ca77d23d4f029028b0c3b
Alex P <alex.pantec@gmail.com>
2019-05-23 15:12:51 +0300

    update tests

--- alexp/src/test/java/alexp/macrobase/pipeline/benchmark/config/BenchmarkConfigTest.java
+++ alexp/src/test/java/alexp/macrobase/pipeline/benchmark/config/BenchmarkConfigTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Map;
 
 import static org.junit.Assert.*;
@@ -25,109 +24,116 @@ public class BenchmarkConfigTest {
                     "metricColumns", Lists.newArrayList("d1", "d2"),
                     "labelColumn", "is_anomaly"
             ),
-            "algorithm", ImmutableMap.of(
-                    "id", "iforest",
-                    "parameters", ImmutableMap.of(
-                            "treesCount", 100,
-                            "param2", 3.14
-                    ))
-    );
-
-    private final Map<String, Object> config2Values = ImmutableMap.of(
-            "dataset", ImmutableMap.of(
-                    "uri", "csv://mulcross.csv",
-                    "id", "mulcross dataset",
-                    "metricColumns", Lists.newArrayList("d1", "d2")
-                    // no label
-            ),
-            "algorithm", ImmutableMap.of(
-                    "id", "iforest",
-                    "parameters", ImmutableMap.of(
-                            "treesCount", 100,
-                            "param2", 3.14
-                    ))
-    );
-
-    private final Map<String, Object> configGsValues = ImmutableMap.of(
-            "dataset", ImmutableMap.of(
-                    "uri", "csv://shuttle.csv",
-                    "id", "shuttle dataset",
-                    "metricColumns", Lists.newArrayList("d1", "d2"),
-                    "labelColumn", "is_anomaly"
-            ),
-            "algorithm", ImmutableMap.of(
-                    "id", "iforest",
-                    "parameters", ImmutableMap.of(
-                            "treesCount", 100,
-                            "param2", 3.14
-                    ),
-                    "gridsearch", ImmutableMap.of(
-                            "measure", "pr",
+            "classifiers", Lists.newArrayList(
+                    ImmutableMap.of(
+                            "id", "iforest",
                             "parameters", ImmutableMap.of(
-                                    "treesCount", Lists.newArrayList(50, 100, 150),
-                                    "param3", Lists.newArrayList(1.0, 2.0, 2.5, 3.0),
-                                    "param4", Lists.newArrayList("mode1", "mode2")
-                            )
-                    ))
+                                    "treesCount", 100,
+                                    "param2", 3.14
+                            ),
+                            "gridsearch", ImmutableMap.of(
+                                    "measure", "pr",
+                                    "parameters", ImmutableMap.of(
+                                            "treesCount", Lists.newArrayList(50, 100, 150),
+                                            "param3", Lists.newArrayList(1.0, 2.0, 2.5, 3.0),
+                                            "param4", Lists.newArrayList("mode1", "mode2")
+                                    )
+                            )),
+                    ImmutableMap.of("id", "fastmcd")
+            )
     );
 
-//    @Test
-//    public void loadsFromMap() {
-//        BenchmarkConfig config = BenchmarkConfig.load(new StringObjectMap(configValues));
-//
-//        assertEquals("csv://shuttle.csv", config.getDatasetConfig().getUri().getOriginalString());
-//        assertEquals("shuttle dataset", config.getDatasetConfig().getDatasetId());
-//        assertEquals("is_anomaly", config.getDatasetConfig().getLabelColumn());
-//        assertArrayEquals(new String[]{"d1", "d2"}, config.getDatasetConfig().getMetricColumns());
-//        assertEquals("iforest", config.getClassifierConfig().getAlgorithmId());
-//        assertEquals(ImmutableMap.of(
-//                "treesCount", 100,
-//                "param2", 3.14
-//        ), config.getClassifierConfig().getParameters().getValues());
-//        assertNull(config.getClassifierConfig().getGridSearchConfig());
-//    }
-
     @Test
-    public void savesToMap() {
-        assertEquals(configValues, BenchmarkConfig.load(new StringObjectMap(configValues)).toMap().getValues());
+    public void loadsFromMap() throws IOException {
+        BenchmarkConfig config = BenchmarkConfig.load(new StringObjectMap(configValues));
+
+        assertEquals("csv://shuttle.csv", config.getDatasetConfig().getUri().getOriginalString());
+        assertEquals("shuttle dataset", config.getDatasetConfig().getDatasetId());
+        assertEquals("is_anomaly", config.getDatasetConfig().getLabelColumn());
+        assertArrayEquals(new String[]{"d1", "d2"}, config.getDatasetConfig().getMetricColumns());
+
+        AlgorithmConfig alg1 = config.getClassifierConfig("iforest");
+        assertEquals("iforest", alg1.getAlgorithmId());
+        assertEquals(ImmutableMap.of(
+                "treesCount", 100,
+                "param2", 3.14
+        ), alg1.getParameters().getValues());
+        GridSearchConfig gsConfig = alg1.getGridSearchConfig();
+        assertNotNull(gsConfig);
+        assertEquals("pr", gsConfig.getMeasure());
+        assertArrayEquals(Lists.newArrayList(50, 100, 150).toArray(), gsConfig.getParameters().get("treesCount"));
+        assertArrayEquals(Lists.newArrayList(1.0, 2.0, 2.5, 3.0).toArray(), gsConfig.getParameters().get("param3"));
+        assertArrayEquals(Lists.newArrayList("mode1", "mode2").toArray(), gsConfig.getParameters().get("param4"));
+
+        AlgorithmConfig alg2 = config.getClassifierConfig("fastmcd");
+        assertEquals("fastmcd", alg2.getAlgorithmId());
+        assertEquals(StringObjectMap.empty().getValues(), alg2.getParameters().getValues());
+        assertNull(alg2.getGridSearchConfig());
+
+        try {
+            config.getClassifierConfig("notexisting");
+            fail("getClassifierConfig should throw");
+        } catch (Exception ignored) {}
     }
 
     @Test
-    public void worksWithoutLabel() {
-        BenchmarkConfig config = BenchmarkConfig.load(new StringObjectMap(config2Values));
+    public void retrievesExecutionConfig() throws IOException {
+        BenchmarkConfig mainConfig = BenchmarkConfig.load(new StringObjectMap(configValues));
 
-        assertEquals("csv://mulcross.csv", config.getDatasetConfig().getUri().getOriginalString());
-        assertNull(config.getDatasetConfig().getLabelColumn());
+        ExecutionConfig config = mainConfig.getExecutionConfig("iforest");
 
-        assertEquals(config2Values, BenchmarkConfig.load(new StringObjectMap(config2Values)).toMap().getValues());
+        assertEquals("csv://shuttle.csv", config.getDatasetConfig().getUri().getOriginalString());
+        assertEquals("shuttle dataset", config.getDatasetConfig().getDatasetId());
+        assertEquals("is_anomaly", config.getDatasetConfig().getLabelColumn());
+        assertArrayEquals(new String[]{"d1", "d2"}, config.getDatasetConfig().getMetricColumns());
+
+        AlgorithmConfig alg = config.getClassifierConfig();
+        assertEquals("iforest", alg.getAlgorithmId());
+        assertEquals(ImmutableMap.of(
+                "treesCount", 100,
+                "param2", 3.14
+        ), alg.getParameters().getValues());
+        GridSearchConfig gsConfig = alg.getGridSearchConfig();
+        assertNotNull(gsConfig);
+        assertEquals("pr", gsConfig.getMeasure());
+        assertArrayEquals(Lists.newArrayList(50, 100, 150).toArray(), gsConfig.getParameters().get("treesCount"));
+        assertArrayEquals(Lists.newArrayList(1.0, 2.0, 2.5, 3.0).toArray(), gsConfig.getParameters().get("param3"));
+        assertArrayEquals(Lists.newArrayList("mode1", "mode2").toArray(), gsConfig.getParameters().get("param4"));
     }
 
-//    @Test
-//    public void loadsSavesGs() {
-//        BenchmarkConfig config = BenchmarkConfig.load(new StringObjectMap(configGsValues));
-//
-//        GridSearchConfig gsConfig = config.getClassifierConfig().getGridSearchConfig();
-//        assertNotNull(gsConfig);
-//        assertEquals("pr", gsConfig.getMeasure());
-//        assertArrayEquals(Lists.newArrayList(50, 100, 150).toArray(), gsConfig.getParameters().get("treesCount"));
-//        assertArrayEquals(Lists.newArrayList(1.0, 2.0, 2.5, 3.0).toArray(), gsConfig.getParameters().get("param3"));
-//        assertArrayEquals(Lists.newArrayList("mode1", "mode2").toArray(), gsConfig.getParameters().get("param4"));
-//
-//        assertEquals(configGsValues, BenchmarkConfig.load(new StringObjectMap(configGsValues)).toMap().getValues());
-//    }
-
     @Test
-    public void savesToFile() throws IOException {
-        String inputFilePath = tmpFolder.getRoot().getAbsolutePath() + "/config.yaml";
-        new StringObjectMap(configGsValues).toYamlFile(inputFilePath);
+    public void savesResultToFile() throws IOException {
+        BenchmarkConfig mainConfig = BenchmarkConfig.load(new StringObjectMap(configValues));
 
-        BenchmarkConfig loadedConfig = BenchmarkConfig.load(StringObjectMap.fromYamlFile(inputFilePath));
-        assertEquals(configGsValues, loadedConfig.toMap().getValues());
+        ExecutionConfig config = mainConfig.getExecutionConfig("iforest");
+
+        Map<String, Object> expectedResultConfigValues = ImmutableMap.of(
+                "dataset", ImmutableMap.of(
+                        "uri", "csv://shuttle.csv",
+                        "id", "shuttle dataset",
+                        "metricColumns", Lists.newArrayList("d1", "d2"),
+                        "labelColumn", "is_anomaly"
+                ),
+                "classifier", ImmutableMap.of(
+                        "id", "iforest",
+                        "parameters", ImmutableMap.of(
+                                "treesCount", 100,
+                                "param2", 3.14
+                        ),
+                        "gridsearch", ImmutableMap.of(
+                                "measure", "pr",
+                                "parameters", ImmutableMap.of(
+                                        "treesCount", Lists.newArrayList(50, 100, 150),
+                                        "param3", Lists.newArrayList(1.0, 2.0, 2.5, 3.0),
+                                        "param4", Lists.newArrayList("mode1", "mode2")
+                                )
+                        ))
+        );
 
         String resultFilePath = tmpFolder.getRoot().getAbsolutePath() + "/config.json";
-        loadedConfig.toMap().toJsonFile(resultFilePath);
+        config.toMap().toJsonFile(resultFilePath);
 
-        BenchmarkConfig loadedConfig2 = BenchmarkConfig.load(StringObjectMap.fromJsonFile(resultFilePath));
-        assertEquals(configGsValues, loadedConfig2.toMap().getValues());
+        StringObjectMap loadedConfig = StringObjectMap.fromJsonFile(resultFilePath);
+        assertEquals(expectedResultConfigValues, loadedConfig.getValues());
     }
 }
