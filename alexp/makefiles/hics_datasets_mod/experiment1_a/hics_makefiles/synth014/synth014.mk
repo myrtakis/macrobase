8d46a5326b47c059baed3e4192fe7bec02cdc199
nikos_myrtakis <myrtakis@csd.uoc.gr>
2019-07-17 18:10:54 +0200

    makefiles for dimensionality 23

--- alexp/makefiles/hics_datasets_mod/experiment1_a/hics_makefiles/synth014/synth014.mk
+++ alexp/makefiles/hics_datasets_mod/experiment1_a/hics_makefiles/synth014/synth014.mk
@@ -1,6 +1,6 @@
-JAVA_COMMAND = "java -jar alexp/target/benchmark.jar"
-CONFIG_PATH = "alexp/data/explanation/experiments/hics_mod_datasets/experiment1_a/hics_configs/synthmod014"
-OUTPUT_PATH = "alexp/explanationExp/hics_datasets_modified/experiment1_a"
+JAVA_COMMAND = java -jar alexp/target/benchmark.jar
+CONFIG_PATH = alexp/data/explanation/experiments/hics_mod_datasets/experiment1_a/hics_configs/synthmod014
+OUTPUT_PATH = alexp/explanationExp/hics_datasets_modified/experiment1_a
 BENCH_COMMAND = "$(JAVA_COMMAND) -b $(CONFIG_PATH) --e --so $(OUTPUT_PATH)"
 all:
 	tmux new -d -s mod_hics_data_exp1a_hics_14
