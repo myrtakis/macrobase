fa0a98be9765c7919eaf0ae25fb5fcc704833428
nikos_myrtakis <myrtakis@csd.uoc.gr>
2019-07-18 10:22:25 +0200

    Fix config path for 100 dims

--- alexp/makefiles/hics_datasets_mod/experiment1_a/hics_makefiles/synth100/synth100_lof.mk
+++ alexp/makefiles/hics_datasets_mod/experiment1_a/hics_makefiles/synth100/synth100_lof.mk
@@ -1,5 +1,5 @@
 JAVA_COMMAND = java -jar alexp/target/benchmark.jar
-CONFIG_PATH = alexp/data/explanation/experiments/hics_mod_datasets/experiment1_a/hics_configs/synthmod/100/lof
+CONFIG_PATH = alexp/data/explanation/experiments/hics_mod_datasets/experiment1_a/hics_configs/synthmod100/lof
 OUTPUT_PATH = alexp/explanationExp/hics_datasets_modified/experiment1_a
 BENCH_COMMAND = "$(JAVA_COMMAND) -b $(CONFIG_PATH) --e --so $(OUTPUT_PATH)"
 all:
