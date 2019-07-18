c28c0775164fb29ee7c1d849338bcda4d2fed230
nikos_myrtakis <myrtakis@csd.uoc.gr>
2019-07-18 13:36:24 +0200

    Lookout configs and makefiles added

---
+++ alexp/makefiles/hics_datasets_mod/experiment1_a/lookout_makefiles/synth023/synth023_2D.mk
@@ -0,0 +1,8 @@
+JAVA_COMMAND = java -jar alexp/target/benchmark.jar
+CONFIG_PATH = alexp/data/explanation/experiments/hics_mod_datasets/experiment1_a/lookout_configs/synthmod023/2D_B50
+OUTPUT_PATH = alexp/explanationExp/hics_datasets_modified/experiment1_a
+BENCH_COMMAND = "$(JAVA_COMMAND) -b $(CONFIG_PATH) --e --so $(OUTPUT_PATH)"
+all:
+	tmux new -d -s mod_hics_data_exp1a_lookout_23_2d
+	tmux send-keys -t mod_hics_data_exp1a_lookout_23_2d.0 "cd ~/Documents/macrobase" ENTER
+	tmux send-keys -t mod_hics_data_exp1a_lookout_23_2d.0 $(BENCH_COMMAND) ENTER
