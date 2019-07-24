7075da833fc5f9b31617fa38f4f2e67866d7329c
nikos_myrtakis <myrtakis@csd.uoc.gr>
2019-07-24 11:50:09 +0200

    Candidate cutoff = 600 configs added. Configs reorganization

---
+++ alexp/makefiles/hics_datasets_mod/experiment1_a/hics_makefiles/c600/synth039/synth039_lof.mk
@@ -0,0 +1,8 @@
+JAVA_COMMAND = java -jar alexp/target/benchmark.jar
+CONFIG_PATH = alexp/data/explanation/experiments/hics_mod_datasets/experiment1_a/hics_configs/c600/synthmod039/lof
+OUTPUT_PATH = alexp/explanationExp/hics_datasets_modified/experiment1_a
+BENCH_COMMAND = "$(JAVA_COMMAND) -b $(CONFIG_PATH) --e --so $(OUTPUT_PATH)"
+all:
+	tmux new -d -s mod_hics_data_exp1a_hics_c600_39_lof
+	tmux send-keys -t mod_hics_data_exp1a_hics_c600_39_lof.0 "cd ~/Documents/macrobase" ENTER
+	tmux send-keys -t mod_hics_data_exp1a_hics_c600_39_lof.0 $(BENCH_COMMAND) ENTER
