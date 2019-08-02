999154cd9e6767b68db4b64eba78885c0b5426b6
nikos_myrtakis <myrtakis@csd.uoc.gr>
2019-08-02 11:58:12 +0200

    hics makefiles c400 in session name added

--- alexp/makefiles/hics_datasets_mod/experiment1_a/hics_makefiles/c400/synth023/synth023_iforest.mk
+++ alexp/makefiles/hics_datasets_mod/experiment1_a/hics_makefiles/c400/synth023/synth023_iforest.mk
@@ -3,6 +3,6 @@ CONFIG_PATH = alexp/data/explanation/experiments/hics_mod_datasets/experiment1_a
 OUTPUT_PATH = alexp/explanationExp/hics_datasets_modified/experiment1_a
 BENCH_COMMAND = "$(JAVA_COMMAND) -b $(CONFIG_PATH) --e --so $(OUTPUT_PATH)"
 all:
-	tmux new -d -s mod_hics_data_exp1a_hics_23_iforest
-	tmux send-keys -t mod_hics_data_exp1a_hics_23_iforest.0 "cd ~/Documents/macrobase" ENTER
-	tmux send-keys -t mod_hics_data_exp1a_hics_23_iforest.0 $(BENCH_COMMAND) ENTER
+	tmux new -d -s mod_hics_data_exp1a_hics_c400_23_iforest
+	tmux send-keys -t mod_hics_data_exp1a_hics_c400_23_iforest.0 "cd ~/Documents/macrobase" ENTER
+	tmux send-keys -t mod_hics_data_exp1a_hics_c400_23_iforest.0 $(BENCH_COMMAND) ENTER
