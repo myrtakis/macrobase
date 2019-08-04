1590d7b3974bc9e22ded2aa40f0f6a5fb9089fef
nikos_myrtakis <myrtakis@csd.uoc.gr>
2019-08-04 13:59:18 +0200

    Lookout configs for 70D dataset correction

--- alexp/makefiles/hics_datasets_mod/experiment1_a/lookout_makefiles/synth070/synth070_3D_lof.mk
+++ alexp/makefiles/hics_datasets_mod/experiment1_a/lookout_makefiles/synth070/synth070_3D_lof.mk
@@ -1,8 +1,8 @@
 JAVA_COMMAND = java -jar alexp/target/benchmark.jar
-CONFIG_PATH = alexp/data/explanation/experiments/hics_mod_datasets/experiment1_a/lookout_configs/synthmod070/5D_B50/lof
+CONFIG_PATH = alexp/data/explanation/experiments/hics_mod_datasets/experiment1_a/lookout_configs/synthmod070/3D_B50/lof
 OUTPUT_PATH = alexp/explanationExp/hics_datasets_modified/experiment1_a
 BENCH_COMMAND = "$(JAVA_COMMAND) -b $(CONFIG_PATH) --e --so $(OUTPUT_PATH)"
 all:
-	tmux new -d -s mod_hics_data_exp1a_lookout_70_5d_lof
-	tmux send-keys -t mod_hics_data_exp1a_lookout_70_5d_lof.0 "cd ~/Documents/macrobase" ENTER
-	tmux send-keys -t mod_hics_data_exp1a_lookout_70_5d_lof.0 $(BENCH_COMMAND) ENTER
+	tmux new -d -s mod_hics_data_exp1a_lookout_70_3d_lof
+	tmux send-keys -t mod_hics_data_exp1a_lookout_70_3d_lof.0 "cd ~/Documents/macrobase" ENTER
+	tmux send-keys -t mod_hics_data_exp1a_lookout_70_3d_lof.0 $(BENCH_COMMAND) ENTER
