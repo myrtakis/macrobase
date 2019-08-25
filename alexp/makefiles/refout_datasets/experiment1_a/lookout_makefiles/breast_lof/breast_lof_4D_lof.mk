8d3da760f2de23686eb2b88d484272636df9cc67
nikos_myrtakis <myrtakis@csd.uoc.gr>
2019-08-26 00:31:08 +0200

    Breast dataset added with hics / lookout confs, makefiles

---
+++ alexp/makefiles/refout_datasets/experiment1_a/lookout_makefiles/breast_lof/breast_lof_4D_lof.mk
@@ -0,0 +1,8 @@
+JAVA_COMMAND = java -jar alexp/target/benchmark.jar
+CONFIG_PATH = alexp/data/explanation/experiments/refout_datasets/experiment1_a/lookout_configs/breast_lof/4D_B50/lof
+OUTPUT_PATH = alexp/explanationExp/refout_datasets/experiment1_a
+BENCH_COMMAND = "$(JAVA_COMMAND) -b $(CONFIG_PATH) --e --so $(OUTPUT_PATH)"
+all:
+	tmux new -d -s mod_refout_data_exp1a_lookout_31_4d_lof
+	tmux send-keys -t mod_refout_data_exp1a_lookout_31_4d_lof.0 "cd ~/Documents/macrobase" ENTER
+	tmux send-keys -t mod_refout_data_exp1a_lookout_31_4d_lof.0 $(BENCH_COMMAND) ENTER
